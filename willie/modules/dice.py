973e7885d142b5d68229cebb568c6fbdef97322f
QuillOmega0 <QuillOmega0@gmail.com>
2015-07-10 23:30:20 -0700

    Stripping out redundancy, adding bitchiness
    Bot doesn't need to represent options.

--- willie/modules/dice.py
+++ willie/modules/dice.py
@@ -226,10 +226,10 @@ def choose(bot, trigger):
     .choice option1|option2|option3 - Makes a difficult choice easy.
     """
     if not trigger.group(2):
-        return bot.reply('I\'d choose an option, but you didn\'t give me any.')
+        return bot.reply('You didn\'t even provide me with an option!')
     choices = re.split('[\|\\\\\/]', trigger.group(2))
     pick = random.choice(choices)
-    return bot.reply('Your options: %s. My choice: %s' % (', '.join(choices), pick))
+    return bot.reply('Because you\'re indecisive, I pick: %s' % (', '.join(choices), pick))
 
 
 if __name__ == "__main__":
